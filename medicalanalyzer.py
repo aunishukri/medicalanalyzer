241de374a039d7de7152d242caf0eb89fa44b1a3
aunishukri <163102096+aunishukri@users.noreply.github.com>
2025-01-27 20:09:03 +0800

    Update medicalanalyzer.py

--- medicalanalyzer.py
+++ medicalanalyzer.py
@@ -573,7 +573,7 @@
             "_model_module": "@jupyter-widgets/controls",
             "_model_module_version": "1.5.0",
             "_model_name": "FloatProgressModel",
-            "_view_count": null,
+            "_view_count": None,
             "_view_module": "@jupyter-widgets/controls",
             "_view_module_version": "1.5.0",
             "_view_name": "ProgressView",
